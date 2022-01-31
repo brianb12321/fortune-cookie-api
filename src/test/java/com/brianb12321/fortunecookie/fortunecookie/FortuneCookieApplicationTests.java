93d7d3a3485e22d7afb23f08d1086d6d12c15a64
brianb12321 <brian.jx.b@gmail.com>
2022-01-30 21:44:22 -0600

    Initialized project

---
+++ src/test/java/com/brianb12321/fortunecookie/fortunecookie/FortuneCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianb12321.fortunecookie.fortunecookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneCookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
